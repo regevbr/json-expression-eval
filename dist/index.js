48090901b5a7dd335c6bbbf5d13a7a3a0914f08b
regevbr <regevbr@gmail.com>
2018-06-09 16:53:21 +0300

    fix testing

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var _ = require("underscore");
+const _ = require("underscore");
 function isAndOp(expression) {
     return expression.and !== undefined;
 }
@@ -10,45 +10,45 @@ function isOrOp(expression) {
 function isNotOp(expression) {
     return expression.not !== undefined;
 }
-exports.evaluate = function (expression, context, functionsTable) {
-    var _evaluate = function (_expression) {
-        var keys = Object.keys(_expression);
+exports.evaluate = (expression, context, functionsTable) => {
+    const _evaluate = (_expression) => {
+        const keys = Object.keys(_expression);
         if (keys.length !== 1) {
             throw new Error('Invalid expression - too may keys');
         }
-        var key = keys[0];
+        const key = keys[0];
         if (isAndOp(_expression)) {
-            var andExpression = _expression.and;
+            const andExpression = _expression.and;
             if (andExpression.length === 0) {
                 throw new Error('Invalid expression - and operator must have at least one expression');
             }
-            var result_1 = true;
-            _.each(andExpression, function (currExpression) {
-                var currResult = _evaluate(currExpression);
-                result_1 = result_1 && currResult;
+            let result = true;
+            _.each(andExpression, (currExpression) => {
+                const currResult = _evaluate(currExpression);
+                result = result && currResult;
             });
-            return result_1;
+            return result;
         }
         else if (isOrOp(_expression)) {
-            var orExpression = _expression.or;
+            const orExpression = _expression.or;
             if (orExpression.length === 0) {
                 throw new Error('Invalid expression - or operator must have at least one expression');
             }
-            var result_2 = false;
-            _.each(orExpression, function (currExpression) {
-                var currResult = _evaluate(currExpression);
-                result_2 = result_2 || currResult;
+            let result = false;
+            _.each(orExpression, (currExpression) => {
+                const currResult = _evaluate(currExpression);
+                result = result || currResult;
             });
-            return result_2;
+            return result;
         }
         else if (isNotOp(_expression)) {
-            var notExpression = _expression.not;
+            const notExpression = _expression.not;
             return !_evaluate(notExpression);
         }
         else if (key in functionsTable) {
             return functionsTable[key](_expression[key], context);
         }
-        throw new Error("Invalid expression - unknown function " + key);
+        throw new Error(`Invalid expression - unknown function ${key}`);
     };
     return _evaluate(expression);
 };
